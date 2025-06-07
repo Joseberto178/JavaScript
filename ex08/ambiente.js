36902dcdd59734c78269be9eddea89cd98d3534e
Joseberto178 <josebertomendonca@gmail.com>
2025-06-07 17:49:30 -0300

    atualizaçap

--- ex08/ambiente.js
+++ ex08/ambiente.js
@@ -1,5 +1,20 @@
-var c = 1
-while (c <=6) {
-    console.log("tudo bem")
-    c++
-}
+// Arrays
+const numeros = [1, 4, 9, 2, 3, 6, 20];
+Math.max(...numeros); // retorna 20
+
+// Transformar ArrayLike and Array
+const items = document.querySelectorAll('li');
+
+// items.map() não existe, agora [...items]
+// é uma nova array, com cada elemento de items.
+[...items].map((item) => (item.innerText = 'Teste'));
+
+// Objetos
+const carro = {
+  cor: 'azul',
+  portas: 4,
+  ano: 2020,
+};
+
+const cloneCarro = { ...carro };
+const carroEsportivo = { turbo: true, ...carro };
